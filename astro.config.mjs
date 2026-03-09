0fb0a01a301384b05d933dd9e16b725d4a4e8b43
Austin Smith <austindsmith_accounts@proton.me>
2026-03-08 20:03:27 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "thewhitelodge.org",
+	site: "https://thewhitelodge.org",
 	integrations: [
 		starlight({
 			title: 'My Docs',
