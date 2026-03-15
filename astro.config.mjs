d0bfb4d2429ee58ea937fb960f3518d3f82a4b2c
Austin Smith <austin.smith@ped.nm.gov>
2026-03-14 22:25:51 -0600

    set up some structure, created inventories, established theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import gruvbox from "starlight-theme-gruvbox";
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://thedocslodge.org",
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: "https://thedocslodge.org",
+    integrations: [starlight({
+        plugins: [
+            gruvbox()
+        ],
+        title: 'The Docs Lodge',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/austindsmith' }],
+        sidebar: [
+            {
+                label: 'Phases',
+                autogenerate: { directory: 'phases' },
+            },
+            {
+                label: 'Decisions',
+                autogenerate: { directory: 'decisions' },
+            },
+            {
+                label: 'Runbooks',
+                autogenerate: { directory: 'runbooks' },
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+    }), mdx()],
 });
